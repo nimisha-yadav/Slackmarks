b3fd8a46d2e7d84049c06a1dd591f9428518f847
Nimisha Yadav <iamnimishayadav@gmail.com>
2021-07-11 21:42:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const token = "xoxb-2255208467638-2247237629479-HjHlnU018vKWemvCMxFKBwZz";    //starting with `xoxb`
+const token = "xoxb-2255208467638-2247237629479-VgSgMszqgQcTmlJbKjziuD7J";    //starting with `xoxb`
 const channel = "@nimisha-yadav";    // e.g. @john_wayne
 const slackUrl = "https://slack.com/api/chat.postMessage";    // Magic Url
