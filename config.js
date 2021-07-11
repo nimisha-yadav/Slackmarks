923fef47e58be481fee2204a667bfa1f7d45952c
Nimisha Yadav <iamnimishayadav@gmail.com>
2021-07-11 21:03:27 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+const token = "<YOUR_CUSTOM_SLACK_BOT_TOKEN>";    //starting with `xoxb`
+const channel = "@<YOUR_SLACK_USERNAME>";    // e.g. @john_wayne
+const slackUrl = "https://slack.com/api/chat.postMessage";    // Magic Url
