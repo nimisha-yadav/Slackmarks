c24ede0faf01be43e25613fe81b68be5497bf04a
Nimisha Yadav <iamnimishayadav@gmail.com>
2021-07-11 22:21:04 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+const token = "xoxb-2247283481575-2274666518609-cI0hPpjxb9xDSyYASdfO7IKr";    //starting with `xoxb`
+const channel = "@iamnimishayadav";    // e.g. @john_wayne
+const slackUrl = "https://slack.com/api/chat.postMessage";    // Magic Url
